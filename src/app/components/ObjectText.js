5c0a1e908e9985bb83b98d0c871fbaf1a62646fc
michael-mason <cptshiner@gmail.com>
2017-10-11 16:10:43 +1100

    object text now own component
    - object text now own component
    - tweak to modal transition

---
+++ src/app/components/ObjectText.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import nl2br from 'nl2br';
+
+export default function ObjectText(props) {
+  const { title, description } = props;
+
+  return (
+    <div className="text">
+      <h1>{title}</h1>
+      <div className="description" dangerouslySetInnerHTML={{ __html: nl2br(description) }} />
+    </div>
+  );
+}
+
+ObjectText.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
--- src/app/containers/ObjectModal.js
+++ src/app/containers/ObjectModal.js
@@ -5,39 +5,26 @@ import { connect } from 'react-redux';
 import CSSTransition from 'react-transition-group/CSSTransition';
 import TransitionGroup from 'react-transition-group/TransitionGroup';
 import BodyClassName from 'react-body-classname';
-import autoBind from 'react-autobind';
-import nl2br from 'nl2br';
 import classnames from 'classnames';
 import * as systemActions from '../actions/system';
 import CloseModalButton from '../components/CloseModalButton';
 import ObjectImage from '../components/ObjectImage';
+import ObjectText from '../components/ObjectText';
 
 class ObjectModal extends Component {
-  constructor(props) {
-    super(props);
-    autoBind(this);
-
-    this.state = {
-      transitioning: false,
-    };
-  }
-
   render() {
     const { actions, activeObject, imageExpanded } = this.props;
 
     return (
       <TransitionGroup>
         {activeObject &&
-        <CSSTransition classNames="fade" timeout={{ enter: 400, exit: 300 }} key={activeObject.id} in={!!activeObject}>
+        <CSSTransition classNames="fade" timeout={400} key={activeObject.id} in={!!activeObject}>
           <BodyClassName className="object-selected">
             <div className={classnames('object-modal', { 'image-expanded': imageExpanded })} onClick={actions.closeObject}>
               <div className="details" onClick={e => e.stopPropagation()}>
                 <CloseModalButton closeObject={actions.closeObject} />
                 <ObjectImage media={activeObject.media} toggleImageExpand={actions.toggleImageExpand} imageExpanded={imageExpanded} />
-                <div className="text">
-                  <h1>{activeObject.title}</h1>
-                  <div className="description" dangerouslySetInnerHTML={{ __html: nl2br(activeObject.description) }} />
-                </div>
+                <ObjectText title={activeObject.title} description={activeObject.description} />
               </div>
             </div>
           </BodyClassName>
